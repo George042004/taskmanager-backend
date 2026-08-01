63bf29f8675575905d9e6611793e56ece2d0520f
George042004 <georgemullarm045@gmail.com>
2026-08-01 18:21:14 +0530

    Loader added

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const dbs = ()=>{
-    mongoose.connect('mongodb+srv://georgemullarm045_db_user:MongoGeorge__90_.@cluster0.5vy1j2m.mongodb.net/?appName=Cluster0')
+    mongoose.connect('mongodb+srv://georgemullarm045_db_user:MongoGeorge__90_.@cluster0.5vy1j2m.mongodb.net/Taskmanager?appName=Cluster0')
     .then(()=>{
         console.log("db connceted");
     })
